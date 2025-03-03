8d35c2e1c9e5626ba63b5bb6c608f3cd22caf792
bobbythree <mynameisbobbylewis@hotmail.com>
2025-03-03 17:27:27 -0600

    add ids to form fields

--- src/Components/Admission.jsx
+++ src/Components/Admission.jsx
@@ -4,20 +4,20 @@ export default function AdmissionPage() {
     <>
     <div className="text-2xl text-base-200 p-3">Admission Form</div>
 
-    <fieldset className="fieldset w-xs bg-base-300  border border-base-300 p-4 ml-3 rounded-box">
+    <fieldset id="name" className="fieldset w-xs bg-base-300  border border-base-300 p-4 ml-3 rounded-box">
       <label className="fieldset-label text-gray-700">Patient Name</label>
       <input type="text" className="input" placeholder="Enter name" />
   
-      <label className="fieldset-label text-gray-700">DOB</label>
-      <input type="date" className="input" placeholder="my-awesome-page" />
+      <label id="dob" className="fieldset-label text-gray-700">DOB</label>
+      <input type="date" className="input" />
   
-      <label className="fieldset-label text-gray-700">Admission Date</label>
-      <input type="date" className="input" placeholder="Name" />
+      <label id="admission-date" className="fieldset-label text-gray-700">Admission Date</label>
+      <input type="date" className="input" />
 
-      <label className="fieldset-label text-gray-700">Diagnosis</label>
+      <label id="diagnosis" className="fieldset-label text-gray-700">Diagnosis</label>
       <input type="text" className="input" placeholder="Enter Diagnosis" />
 
-      <label className="fieldset-label text-gray-700">Precautions</label>
+      <label id="precautions" className="fieldset-label text-gray-700">Precautions</label>
       <input type="text" className="input" placeholder="Enter Precautions" />
 
     </fieldset>
